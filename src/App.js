698d0a697f2e1b0b122e0e6ef921e468e690d285
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-11-07 04:53:27 -0300

    separated by color

--- src/App.js
+++ src/App.js
@@ -3,31 +3,44 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+
+  const [player, setPlayer] = useState(0);
   
   const [status, setStatus] = useState(
     {
-      1_1: false, 1_2: false, 1_3: false,
-      2_1: false, 2_2: false, 2_3: false,
-      3_1: false, 3_2: false, 3_3: false
+      1_1: 0, 1_2: 0, 1_3: 0,
+      2_1: 0, 2_2: 0, 2_3: 0,
+      3_1: 0, 3_2: 0, 3_3: 0
     }
   )
 
+  const player_turn = () => {
+    if (!player) return 1
+    return player === 1 ? 2 :  1 
+  }
+
   const handleClick = (pos) => {
-    console.log('status', status[pos])
-    setStatus({...status, [pos]: !status[pos]})
+    if(status[pos]) return;
+    setPlayer(player_turn())
+    setStatus({...status, [pos]: player_turn()})
   }
 
   const cell = (i, j) => {
-    return <div onClick={() => handleClick(`${i}_${j}`)} className={`col ${status[`${i}_${j}`] ? 'pressed' : ''}`}>{i}x{j}</div>
+
+    const pos = `${i}_${j}`;
+    const player_class = `pressed-player-` + status[pos];
+
+    console.log('player class', player_class)
+    return <div key={j} onClick={() => handleClick(pos)} className={`col ${player_class}`}>{i}x{j} Player: {status[pos]}</div>
   }
 
   return (
     <div className="App">
       <header className="App-header"></header>
-      
+      Player: {player}
       {
         [1,2,3].map(i => (
-          <div className='row'> 
+          <div key={i} className='row'> 
             {
               [1,2,3].map(j => (
                 cell(i,j)
